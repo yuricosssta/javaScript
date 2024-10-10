f2f003f81265e1954f5a01dc34111a33d7b80bc8
Yuri Costa <eng.yuricosta@gmail.com>
2024-10-10 13:34:41 -0300

    Adicionar alterar objetos

---
+++ alterar.js
@@ -0,0 +1,13 @@
+const pessoa = {
+    nome: "Luma",
+    profissao: "Engenheira"
+};
+
+console.log(pessoa.nome);
+
+console.log(pessoa.telefone);
+
+pessoa.telefone = "31 99993322"
+
+console.log(pessoa.telefone);
+console.log(pessoa);
